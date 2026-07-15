c18a7cac3a37f50ec6aacce69686c3d042565535
SophearithSaing <sophearithsaing123@gmail.com>
2026-07-16 01:20:54 +0700

    Add basic textarea

--- main.go
+++ main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	tea "charm.land/bubbletea/v2"
+)
+
+func main() {
+	p := tea.NewProgram(initialModel())
+
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
