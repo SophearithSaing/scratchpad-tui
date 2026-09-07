a3914aae57b1ebe7d5694587dff399fd33225b55
SophearithSaing <sophearithsaing123@gmail.com>
2026-09-07 22:24:30 +0700

    Add save and write file

--- store.go
+++ store.go
@@ -66,3 +66,73 @@ func (s sessionStore) loadIndex() (savedSession, error) {
 func (s sessionStore) notePath(id int) string {
 	return filepath.Join(filepath.Dir(s.path), fmt.Sprintf("note-%d.md", id))
 }
+
+func (s sessionStore) save(session savedSession) error {
+	_, err := s.loadIndex()
+	if err != nil && !os.IsNotExist(err) {
+		return fmt.Errorf("error reading previous index: %w", err)
+	}
+
+	current := make(map[int]bool, len(session.Tabs))
+	for _, tab := range session.Tabs {
+		current[tab.ID] = true
+	}
+
+	dir := filepath.Dir(s.path)
+	if err := os.MkdirAll(dir, 0o700); err != nil {
+		return fmt.Errorf("create state directory: %w", err)
+	}
+
+	data, err := json.MarshalIndent(session, "", "  ")
+	if err != nil {
+		return fmt.Errorf("encode session: %w", err)
+	}
+	data = append(data, '\n')
+
+	for _, tab := range session.Tabs {
+		path := s.notePath(tab.ID)
+		err = writeFile(path, []byte(tab.Content), true)
+		if err != nil {
+			return err
+		}
+	}
+	if err := writeFile(s.path, data, true); err != nil {
+		return err
+	}
+	return nil
+}
+
+func writeFile(path string, data []byte, replace bool) error {
+	tmp, err := os.CreateTemp(filepath.Dir(path), ".scratchpad-*")
+	if err != nil {
+		return fmt.Errorf("create temporary file for %s: %w", path, err)
+	}
+	tmpName := tmp.Name()
+	defer os.Remove(tmpName)
+
+	if err := tmp.Chmod(0o600); err != nil {
+		tmp.Close()
+		return fmt.Errorf("secure temporary file for %s: %w", path, err)
+	}
+	if _, err := tmp.Write(data); err != nil {
+		tmp.Close()
+		return fmt.Errorf("write temporary file for %s: %w", path, err)
+	}
+	if err := tmp.Sync(); err != nil {
+		tmp.Close()
+		return fmt.Errorf("sync temporary file for %s: %w", path, err)
+	}
+	if err := tmp.Close(); err != nil {
+		return fmt.Errorf("close temporary file for %s: %w", path, err)
+	}
+
+	if replace {
+		err = os.Rename(tmpName, path)
+	} else {
+		err = os.Link(tmpName, path)
+	}
+	if err != nil {
+		return fmt.Errorf("publish %s: %w", path, err)
+	}
+	return nil
+}
